2d5428b707b8d1d2b245bffd56bdf8a496cb856e
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-12-09 18:45:49 +0100

    [YARP_OS] Use set_option instead of setsockopt

--- src/libYARP_OS/include/yarp/os/impl/TcpStream.h
+++ src/libYARP_OS/include/yarp/os/impl/TcpStream.h
@@ -68,9 +68,9 @@ public:
         return ::recv(sd, buf, n, 0);
     }
 
-    inline ssize_t recv_n (void *buf, size_t n, struct timeval *tv)
-    {
-        setsockopt(sd, SOL_SOCKET, SO_RCVTIMEO, (char *)tv, sizeof (*tv));
+
+    inline ssize_t recv_n (void *buf, size_t n, struct timeval *tv) {
+        set_option(SOL_SOCKET, SO_RCVTIMEO, (char *)tv, sizeof (*tv));
         return ::recv(sd, buf, n, 0);
     }
 
@@ -79,9 +79,8 @@ public:
         return ::recv(sd, buf, n, 0);
     }
 
-    inline ssize_t recv (void *buf, size_t n, struct timeval *tv)
-    {
-        setsockopt(sd, SOL_SOCKET, SO_RCVTIMEO, (char *)tv, sizeof (*tv));
+    inline ssize_t recv (void *buf, size_t n, struct timeval *tv) {
+        set_option(SOL_SOCKET, SO_RCVTIMEO, (char *)tv, sizeof (*tv));
         return ::recv(sd, buf, n, 0);
     }
 
@@ -90,9 +89,8 @@ public:
         return ::send(sd, buf, n, 0);
     }
 
-    inline ssize_t send_n (const void *buf, size_t n, struct timeval *tv)
-    {
-        setsockopt(sd, SOL_SOCKET, SO_SNDTIMEO, (char *)tv, sizeof (*tv));
+    inline ssize_t send_n (const void *buf, size_t n, struct timeval *tv) {
+        set_option(SOL_SOCKET, SO_SNDTIMEO, (char *)tv, sizeof (*tv));
         return ::send(sd, buf, n, 0);
     }
 
